564f7f455f94f1a37340d2e252ae09f000615b42
galit1321 <galit1321@gmail.com>
2016-04-18 18:41:07 +0300

    commit imodalable

---
+++ ex2/IModelable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    interface IModelable
+    {
+        
+            void connect(string ip, int port);
+            void disconnect();
+            void start();
+
+            //proprties here
+            int Port { get; set; }
+            string ip { get; set; }
+            string Order { get; set; }
+            
+            //active method
+            void move(string direction);
+            string getClue();
+            
+
+        
+    }
+}
