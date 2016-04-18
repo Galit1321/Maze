d77ec15eff6faaf4ae7c3ad243adb6c6399de0cb
RevitalS <revitle@mgail.com>
2016-04-18 16:35:51 +0300

    all

---
+++ Server/Game.cs
@@ -0,0 +1,27 @@
+﻿using Server.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Modle
+{
+    /// <summary>
+    /// details of game
+    /// </summary>
+    public class Game
+    {
+        public MazeSolved Maze1 { get; set; }
+        public MazeSolved Maze2 { get; set; }
+        public string Name { get; set; }
+        public int Client1 { get; set; }
+        public int Client2 { get; set; }
+        public string Move { get; set; }
+
+        /// <summary>
+        /// empty constructor
+        /// </summary>
+        public Game() {}
+    }
+}
---
+++ Server/IView.cs
@@ -0,0 +1,23 @@
+﻿using Server.Modle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.View
+{
+    /// <summary>
+    /// interface of view
+    /// </summary>
+    public interface IView
+    {
+        event updateView ViewChanged;
+        /// <summary>
+        /// display data
+        /// </summary>
+        /// <param name="id"> the id </param>
+        /// <param name="massege"> the message </param>
+        void DisplayData(int id, string massege);
+    }
+}
