069e39a6e0d9940efdc4fa1f951ef7df4cc9d4d7
galit1321 <galit1321@gmail.com>
2016-04-18 20:28:17 +0300

    commit Model.cs

---
+++ ex2/Pair.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Pair
+    {
+        public int Row;
+        public int Col;
+        /// <summary>
+        /// construrctor
+        /// </summary>
+        /// <param name="i">row in a matrix</param>
+        /// <param name="j">col in a matrix</param>
+        public Pair (int i,int j)
+        {
+            this.Row = i;
+            this.Col = j;
+        }
+        /// <summary>
+        /// change the row and col according to d 
+        /// </summary>
+        /// <param name="d">the direction we go in martix</param>
+        public void move(string d)
+        {
+            switch (d)
+            {
+                case "up":
+                    this.Row -= 2;
+                    break;
+                case "down":
+                    this.Row += 2;
+                    break;
+                case "right":
+                    this.Col += 2;
+                    break;
+                case "left":
+                    this.Col -= 2;
+                    break;
+                
+            }
+        }
+    }
+}
